443950bf7291af34a8f813c4d1478b53639b3f74
Jait Dixit <jait.dixit@gmail.com>
2014-03-18 18:22:20 +0100

    first attempt

--- lsh/code/mapper.py
+++ lsh/code/mapper.py
@@ -3,17 +3,41 @@
 import numpy as np
 import sys
 
+hash_functions = []
 
-def partition(video_id, shingles):
-    pass
+
+def minhash(video_id, shingles):
+    signature_vector = [1000000000] * 256
+
+    for shingle in shingles:
+        for i, hash in enumerate(hash_functions):
+            if signature_vector[i] > hash[shingle]:
+                signature_vector[i] = hash[shingle]
+
+    # b = 15, r = 17
+    for i in range(0, 15):
+        key = ""
+        key = key + str((100 + i))
+        for j in range(0, 18 if i == 14 else 17):
+            key = key + str(signature_vector[17 * i + j])
+        print '%s\t%s' % (key, video_id)
 
 if __name__ == "__main__":
     # Very important. Make sure that each machine is using the
     # same seed when generating random numbers for the hash functions.
     np.random.seed(seed=42)
 
+    for i in range(0, 256):
+        a = np.random.randint(1000)
+        b = np.random.randint(1000)
+        temp = []
+        for j in range(0, 10001):
+            temp.append((a * j + b) % 10001)
+
+        hash_functions.append(temp)
+
     for line in sys.stdin:
         line = line.strip()
         video_id = int(line[6:15])
-        shingles = np.fromstring(line[16:], sep=" ")
-        partition(video_id, shingles)
+        shingles = np.fromstring(line[16:], dtype=np.int32, sep=" ")
+        minhash(video_id, shingles)
