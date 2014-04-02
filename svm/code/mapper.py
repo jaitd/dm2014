90b040d5818c711894ab3d4e0c603d891c8e961b
Jait Dixit <jait.dixit@gmail.com>
2014-04-02 19:45:57 +0200

    add svm code

--- svm/code/mapper.py
+++ svm/code/mapper.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python2.7
 
+import sys
+
+import numpy as np
+
+from sklearn.linear_model.stochastic_gradient import SGDClassifier
+
+# one classifier for the mapper
+# this is the classifier on which we'll be running the partial_fit
+classifier = SGDClassifier()
+
+
 # This function has to either stay in this form or implement the
 # feature mapping. For details refer to the handout pdf.
 def transform(x_original):
-    return x_original
+    return np.append(x_original, [1])
+
+
+def train(y, features):
+    classifier.partial_fit(features, y, classes=[1, -1])
+
 
 if __name__ == "__main__":
-    pass
+    for line in sys.stdin:
+        line = line.strip()
+        features = np.fromstring(line, dtype=np.float64, sep=" ")
+        train([features[0]], features[1:])
+
+    temp = classifier.coef_[0]
+    temp = np.append(temp, classifier.intercept_)
+
+    value = ' '.join(str(x) for x in temp)
+
+    print '%s\t%s' % ('1', value)
--- svm/code/reducer.py
+++ svm/code/reducer.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python2.7
 
+from __future__ import division
+
+import numpy as np
+import sys
+
+final_weights = [0] * 401
+count = 0
+
 if __name__ == "__main__":
-    pass
+    for line in sys.stdin:
+        line = line.strip()
+        key, weights_string = line.split("\t")
+
+        weights = np.fromstring(weights_string, dtype=np.float64, sep=" ")
+
+        count = count + 1
+        final_weights = [x + y for x, y in zip(final_weights, weights)]
+
+    final_weights = [x / count for x in final_weights]
+
+    print "%s" % ' '.join(str(x) for x in final_weights)
