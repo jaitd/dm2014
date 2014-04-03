14911091afd54411a575fae2ded14764d9fb424b
Jait Dixit <jait.dixit@gmail.com>
2014-04-03 12:23:31 +0200

    comment the code

--- svm/code/mapper.py
+++ svm/code/mapper.py
@@ -10,13 +10,18 @@ from sklearn.linear_model.stochastic_gradient import SGDClassifier
 # this is the classifier on which we'll be running the partial_fit
 classifier = SGDClassifier()
 
+# batch size for the partial_fit
+BATCH_SIZE = 50
 
-# This function has to either stay in this form or implement the
-# feature mapping. For details refer to the handout pdf.
+
+# appended 1 to the original features for centering the data, the
+# classifier also calculates the offset which is appended to
+# the weights
 def transform(x_original):
     return np.append(x_original, [1])
 
 
+# run paritial_fit for the classifier with each picture
 def train(y, features):
     classifier.partial_fit(features, y, classes=[1, -1])
 
@@ -28,6 +33,7 @@ if __name__ == "__main__":
         train([features[0]], features[1:])
 
     temp = classifier.coef_[0]
+    # appending intercept to the calculated weights
     temp = np.append(temp, classifier.intercept_)
 
     value = ' '.join(str(x) for x in temp)
--- svm/code/reducer.py
+++ svm/code/reducer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2.7
 
+# for floating point division
 from __future__ import division
 
 import numpy as np
@@ -15,9 +16,13 @@ if __name__ == "__main__":
 
         weights = np.fromstring(weights_string, dtype=np.float64, sep=" ")
 
+        # count the number of mappers
+        # used to normalize the average weights
         count = count + 1
+        # keep on adding weights from all the mappers to an array
         final_weights = [x + y for x, y in zip(final_weights, weights)]
 
+    # normalize the weights
     final_weights = [x / count for x in final_weights]
 
     print "%s" % ' '.join(str(x) for x in final_weights)
